1ea980ba0892833b1d2e6927ceb432b8202ba79f
Vadym Telyachy <vtelyachy@gmail.com>
2021-05-18 22:50:52 +0300

    added some routes

--- src/api/services/locations.service.ts
+++ src/api/services/locations.service.ts
@@ -5,19 +5,35 @@ import { HttpService } from './http.service';
 class LocationService {
   public constructor(private http: HttpService) {}
 
-  public locations = (query: string) =>
-    this.http.request<MyResponse<Location[]>>({
-      url: 'someurl',
-      data: {
-        query
-      },
-      method: 'POST'
+  public locations = () =>
+    this.http.request<Location[]>({
+      url: '/location',
+      method: 'GET'
     });
 
-  public getUsers = () => {
-    this.http.request<MyResponse<Location[]>>({
-      url: '/users',
+  public locationsByName = (query: string) =>
+    this.http.request<Location[]>({
+      url: `/location/name/${query}`,
       method: 'GET'
+    });
+
+  public locationsById = (id: string) =>
+    this.http.request<Location[]>({
+      url: `/location/${id}`,
+      method: 'GET'
+    });
+
+  public locationsByFilm = (film: string) =>
+    this.http.request<Location[]>({
+      url: `/location/name/${film}`,
+      method: 'GET'
+    });
+
+  public createLocation = (location: Location) => {
+    this.http.request<Location>({
+      url: '/users',
+      method: 'post',
+      data: location
     });
   };
 }
